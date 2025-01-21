6db880990ff910f589f443a586b0a5ff6c7d68d7
Astrus <justinctinl@gmail.com>
2025-01-20 23:11:31 -0500

    started work on a CNN model

--- src/model.py
+++ src/model.py
@@ -1,1 +1,63 @@
-# insert model code here
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# this is just placeholder code for now (mostly gpt :skull:)
+# just wanted to show u im still here and i didnt forget abt u
+# but u probs forgot abt me bc i dont rly talk much in the discord
+# anyways will work on this fr
+# trust
+# unless one of u wanna work on it
+# but ill get this done soon
+# eventualy
+# mayb
+# hopefully
+
+class CNNModel(nn.Module):
+    def __init__(self, num_classes):
+        super(CNNModel, self).__init__()
+
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(
+            in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1
+        )
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(
+            in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1
+        )
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(
+            in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1
+        )
+        self.bn3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(
+            in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1
+        )
+        self.bn4 = nn.BatchNorm2d(256)
+
+        # Pooling layer
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Fully connected layers
+        self.fc1 = nn.Linear(256 * 2 * 2, 512)  # Assuming input images are 32x32
+        self.fc2 = nn.Linear(512, num_classes)
+
+        # Dropout layer
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        # Convolutional layers with ReLU, batch normalization, and pooling
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
+
+        # Flatten the feature maps
+        x = x.view(x.size(0), -1)
+
+        # Fully connected layers with ReLU and dropout
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+
+        return x
